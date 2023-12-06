51d7a4e13d0449f0c72afb39745b07ae61fb7700
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-12-06 13:16:38 -0800

    Inital backend folder and server setup with connection to mongodb

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const mongoDB = process.env.mongoDBURL;
+
+app.get('/', (req, res) => {
+  return res.status(234).send('Welcome to bookstore')
+})
+
+
+
+
+mongoose 
+  .connect(mongoDB)
+  .then(() => {
+  console.log('App connected to database');
+
+  //run server only if DB connection is successful
+  app.listen(3001, () => {
+    console.log(`App is listening to port: 3001`);
+  })
+ 
+  })
+  .catch((err) => {
+  console.log(err);
+  })
