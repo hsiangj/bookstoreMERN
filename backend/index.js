fbce79e9a162250ed002b1c98aed8557cb1fb4fd
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-12-29 15:01:36 -0800

    Add get/put/delete routes

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,56 @@ app.get('/books', async (req, res) => {
   }
 });
 
+// Route to get one book by id
+app.get('/books/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Route to update a book
+app.put('/books/:id', async (req, res) => {
+  try {
+    if(
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) 
+    return res.status(400).send({
+      message: 'Send all required fields: title, author, publishYear',
+    })
+    
+    const {id} = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+    if(!result) return res.status(404).json({ message: 'Book not found'});
+    return res.status(200).send({ message: 'Book updated successfully', result})
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Route to delete a book
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const result = await Book.findByIdAndDelete(id);
+    if(!result) return res.status(404).json({ message: 'Book not found'});
+
+    return res.status(200).send({ message: 'Book deleted successfully'});
+     
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+})
+
 mongoose 
   .connect(mongoDB)
   .then(() => {
